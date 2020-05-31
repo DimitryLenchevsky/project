2f03e16403a237e093e35631e5e4af3fda8ed7af
DimitryLenchevsky <overborsch@gmail.com>
2020-05-31 16:12:13 +0300

    commit

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from testapp.views import test
-
+# http://127.0.0.1:8000/
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', test)
--- src/testapp/views.py
+++ src/testapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 
 def test(request):
-    return HttpResponse("<h1>Hello World!!!</h1>")
+
+    # got data from DB
+    data = [2, 4, 5, 6, 0, 34, 67, 23, 8, 3, 876, 32]
+    r = random.randint(0, 100)
+    context = {'some_value' : r, 'my_data' : data}
+    return render(request, template_name="testapp/test.html", context=context)
