47954b9024dc3a5a447ae6145d8ca921e2ec0583
DimitryLenchevsky <overborsch@gmail.com>
2020-06-01 18:19:39 +0300

    commit

--- src/testapp/views.py
+++ src/testapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 import random
 
 # Create your views here.
@@ -7,8 +8,10 @@ import random
 
 def test(request):
 
+
+    date = datetime.datetime.now()
     # got data from DB
     data = [2, 4, 5, 6, 0, 34, 67, 23, 8, 3, 876, 32]
     r = random.randint(0, 100)
-    context = {'some_value' : r, 'my_data' : data}
+    context = {'some_value' : r, 'my_data' : data, 'my_date' : date}
     return render(request, template_name="testapp/test.html", context=context)
