c030594d6f997383922d0f5993798e12d4f763dc
DimitryLenchevsky <overborsch@gmail.com>
2020-06-03 18:22:29 +0300

    first models and admin

--- src/testapp/models.py
+++ src/testapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    # pk PK + autoincrement not null
+    name = models.CharField(
+        verbose_name="Название Жанра",
+        max_length=100,   
+    )
+    description = models.TextField(
+        verbose_name="Описание Жанра",
+        null=True,
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.name
