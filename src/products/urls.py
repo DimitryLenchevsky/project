5f9e8f440f6b2ff8d4a934f0664cc490b1345806
DimitryLenchevsky <overborsch@gmail.com>
2020-07-13 17:10:40 +0300

    CRUD

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import ProductView
+from .views import ProductView, ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = 'products'
 
 urlpatterns = [
     path('product/<int:pk>', ProductView.as_view(), name='detail'),
+    path('product-list/', ProductListView.as_view(), name='list'),
+    path('product-create/', ProductCreateView.as_view(), name='create'),
+    path('product-update/<int:pk>', ProductUpdateView.as_view(), name='update'),
+    path('product-delete/<int:pk>', ProductDeleteView.as_view(), name='delete'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from . import models
+from django.urls import reverse_lazy
 
 # Create your views here.
 class ProductView(DetailView):
     model = models.Product
     template_name = 'products/templates/detail.html'
+
+
+class ProductListView(ListView):
+    model = models.Product
+    template_name = 'products/templates/list.html'
+
+
+class ProductCreateView(CreateView):
+    model = models.Product
+    template_name = 'products/templates/create.html'
+    fields = ('name', 'author', 'price', 'genre', 'description',)
+
+
+class ProductUpdateView(UpdateView):
+    model = models.Product
+    template_name = 'products/templates/update.html'
+    fields = ('name', 'author', 'price', 'genre', 'description',)
+    def get_success_url(self):
+        return reverse_lazy('products:detail', kwargs={'pk': self.object.pk})
+    
+
+class ProductDeleteView(DeleteView):
+    model = models.Product
+    template_name = 'products/templates/delete.html'
+    success_url = '/products/templates/list.html/'
